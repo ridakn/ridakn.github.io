02a20020c38495f8b1497f2203e7bacb9d9ec040
Rida Khan <rida.khan5@hotmail.com>
2021-06-23 17:13:47 +0400

    Updates

---
+++ precache-manifest.cf0ca6634e0e8331e286c4a30949b90f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "778e82391959afd9a45c21803e3e7b34",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2bbae6fa4172ce3a2c0c",
+    "url": "/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "66e2a7f892a89ad0d49c",
+    "url": "/static/css/main.1321dd01.chunk.css"
+  },
+  {
+    "revision": "2bbae6fa4172ce3a2c0c",
+    "url": "/static/js/2.e3a270fa.chunk.js"
+  },
+  {
+    "revision": "9371b595e371c0d4c4f99812c6aece96",
+    "url": "/static/js/2.e3a270fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "66e2a7f892a89ad0d49c",
+    "url": "/static/js/main.83c51a4f.chunk.js"
+  },
+  {
+    "revision": "c88671eb88bf6c28dee9",
+    "url": "/static/js/runtime-main.6a060e8a.js"
+  },
+  {
+    "revision": "68be1b44bb00c146f5ca21dc7945af0c",
+    "url": "/static/media/Profile.68be1b44.jpg"
+  },
+  {
+    "revision": "55f4ef0baec89dda4bc3eed4bee7bc4f",
+    "url": "/static/media/boeing.55f4ef0b.jpg"
+  },
+  {
+    "revision": "433b371aabb4a133cfc9682923d5d0b3",
+    "url": "/static/media/culture.433b371a.jpg"
+  },
+  {
+    "revision": "5d4d169ec7e28d53d5c95bae3f756960",
+    "url": "/static/media/dell-logo.5d4d169e.jpg"
+  },
+  {
+    "revision": "53c4c973817430ff2ed8403e67f7bbee",
+    "url": "/static/media/motion.53c4c973.png"
+  },
+  {
+    "revision": "33f31b4ce606a697796e2a9585fafeba",
+    "url": "/static/media/resume.33f31b4c.pdf"
+  },
+  {
+    "revision": "167f94d9e466e8adbfe48391437e3842",
+    "url": "/static/media/sentiment.167f94d9.png"
+  },
+  {
+    "revision": "d6698c9280cebf61b105369c173a8732",
+    "url": "/static/media/service.d6698c92.jpg"
+  }
+]);
